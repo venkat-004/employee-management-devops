94d7706377e2560cb4b9f912552337cc444faa19
venkat-004 <galivenkataramanaiah37@gmail.com>
2026-08-24 10:11:35 +0530

    Add employee management project

---
+++ src/main/java/com/example/employee_management_devops/EmployeeManagementDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.employee_management_devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementDevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/employee_management_devops/EmployeeManagementDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employee_management_devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
